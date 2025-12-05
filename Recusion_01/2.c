5edbad04e29a5ab8fce448466d497b35ed95bbb2
pushpendrapatelh3d <pushpendrapatel275@gmail.com>
2025-12-05 23:10:29 +0530

    recursion practice

---
+++ Recusion_01/2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+//sum of numbers from 1 to n
+
+int sum(int n){
+    if(n==0)
+    return 0;
+    return n+sum(n-1);
+}
+int main(){
+    int n;
+    printf("Enter a number :");
+    scanf("%d",&n);
+    printf("Sum of numbers from 1 to  %d : %d",n,sum(n));
+}
