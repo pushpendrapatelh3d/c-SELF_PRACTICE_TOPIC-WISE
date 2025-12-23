cf2f1d47025947676edc9ee55f9f002ded961b8c
pushpendrapatelh3d <pushpendrapatel275@gmail.com>
2025-12-23 20:25:51 +0530

    msg

---
+++ string/2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+
+//"Think outside the box"-->"box the outside Think"
+//reverse the order of words
+
+void revstr(char a[],int start,int e){
+    char temp;
+    for(int i=start;i<e;i++,e--){
+        temp=a[i];
+        a[i]=a[e];
+        a[e]=temp;
+    } 
+    return;
+}
+int main(){
+    char a[]= "Think outside the box";
+    revstr(a,0,strlen(a)-1);
+    int start=0;
+    for(int i=0;i<=strlen(a);i++){
+        if(a[i]==' '||a[i]=='\0')
+        {
+        revstr(a,start,i-1);
+        start=i+1;
+        }
+    }
+    printf("%s",a);
+}
